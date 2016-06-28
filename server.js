539a61aef5a1e9c41d753c9fe44bb52230e58dda
Evan You <yyx990803@gmail.com>
2016-06-28 07:19:34 -0400

    indent

--- server.js
+++ server.js
@@ -8,46 +8,47 @@ const express = require('express')
 const serialize = require('serialize-javascript')
 const createBundleRenderer = require('vue-server-renderer').createBundleRenderer
 
+const renderer = createBundleRenderer(fs.readFileSync('./dist/server-bundle.js', 'utf-8'), {
+  cache: {
+    max: 10000
+  }
+})
+
 const app = express()
-  const renderer = createBundleRenderer(fs.readFileSync('./dist/server-bundle.js', 'utf-8'), {
-    cache: {
-      max: 10000
-    }
-  })
 
-  app.use(express.static(path.resolve(__dirname, 'dist')))
+app.use(express.static(path.resolve(__dirname, 'dist')))
 
-  app.get('*', (req, res) => {
-    const start = Date.now()
-    const context = { url: req.url }
-    const renderStream = renderer.renderToStream(context)
-    let firstChunk = true
+app.get('*', (req, res) => {
+  const start = Date.now()
+  const context = { url: req.url }
+  const renderStream = renderer.renderToStream(context)
+  let firstChunk = true
 
-    res.write('<!DOCTYPE html><body>')
+  res.write('<!DOCTYPE html><body>')
 
-    renderStream.on('data', chunk => {
-      if (firstChunk) {
-        // send down initial store state
-        if (context.initialState) {
-          res.write(`<script>window.__INITIAL_STATE__=${
-            serialize(context.initialState, { isJSON: true })
-          }</script>`)
-        }
-        firstChunk = false
+  renderStream.on('data', chunk => {
+    if (firstChunk) {
+      // send down initial store state
+      if (context.initialState) {
+        res.write(`<script>window.__INITIAL_STATE__=${
+          serialize(context.initialState, { isJSON: true })
+        }</script>`)
       }
-      res.write(chunk)
-    })
-
-    renderStream.on('end', () => {
-      console.log('request used: ' + (Date.now() - start) + 'ms')
-      res.end(`<script src="/client-bundle.js"></script></body>`)
-    })
-
-    renderStream.on('error', err => {
-      throw err
-    })
+      firstChunk = false
+    }
+    res.write(chunk)
   })
 
-  app.listen(3000, () => {
-    console.log('ready at localhost:3000')
+  renderStream.on('end', () => {
+    console.log('request used: ' + (Date.now() - start) + 'ms')
+    res.end(`<script src="/client-bundle.js"></script></body>`)
   })
+
+  renderStream.on('error', err => {
+    throw err
+  })
+})
+
+app.listen(3000, () => {
+  console.log('ready at localhost:3000')
+})
