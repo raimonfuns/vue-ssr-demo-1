344cb2f8724722f4486edfd22d773859bf24a8c6
Evan You <yyx990803@gmail.com>
2016-06-28 07:00:33 -0400

    add a few babel plugins so that it works in safari

--- src/components/table.js
+++ src/components/table.js
@@ -2,10 +2,10 @@ import Row from './row'
 import store from '../store'
 
 function generateGrid (rowCount, columnCount) {
-  var grid = []
-  for (var r = 0; r < rowCount; r++) {
+  const grid = []
+  for (let r = 0; r < rowCount; r++) {
     var row = { id: r, items: [] }
-    for (var c = 0; c < columnCount; c++) {
+    for (let c = 0; c < columnCount; c++) {
       row.items.push({ id: (r + '-' + c) })
     }
     grid.push(row)
